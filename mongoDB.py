71bb49b3438ab6dad85037c69965c9b9178fb782
Martin Rios-Cardenas <59406376+themexpride@users.noreply.github.com>
2021-11-25 13:16:49 -0800

    fix DB code

--- mongoDB.py
+++ mongoDB.py
@@ -1,7 +1,7 @@
 from pymongo import MongoClient
 from pprint import pprint
 
-client = MongoClient(mongodb+srv://mrioscardenas:%Imtex8311@cluster0.ftnor.mongodb.net/userDB?retryWrites=true&w=majority)
+client = MongoClient("mongodb+srv://mrioscardenas:%Imtex8311@cluster0.ftnor.mongodb.net/userDB?retryWrites=true&w=majority")
 db = client.admin
 
 serverStatusResult = db.command("serverStatus")
