96909043a049fcbaf01d63cdc5a34d320ccc1afb
Martin Rios-Cardenas <59406376+themexpride@users.noreply.github.com>
2021-11-26 17:03:33 -0800

    edited main.py

--- main.py
+++ main.py
@@ -18,44 +18,6 @@ class Search(commands.Bot):
     self.message2 = "Bot still online"
     self.add_commands()
 
-  def search(to_search):
-    #input anime you want to search
-    anime = to_search.upper()
-
-    #read csv, and split on "," the line
-    csv_file = csv.reader(open('anime.csv', "r"), delimiter=",")
-
-    flag = 0
-    # print("Search Results:")
-    results = []
-    #loop through the csv list
-    for row in csv_file:
-        #if current row's 2nd value is equal to input, print that row
-        if anime in row[1].upper():
-            flag = 1
-            results.append(row[1])
-
-    #print numbered search result list
-    for number, anime in enumerate(results):
-        print(number+1, anime)
-
-    #add anime to user list
-    if flag == 0:
-      print("Not found!\n")
-    else:
-      list_num = int(input("Which anime would you like to add to your list?"))
-      user_choice = results[list_num - 1]
-      print("\nAdded " + str(user_choice) + " to your watched list.\n")
-
-  # @client.event
-  async def on_message(message):
-    # if message.author == client.user:
-    #     return
-
-    if message.content == '99!':
-        # response = random.choice(brooklyn_99_quotes)
-        await message.channel.send(response)
-
 class MyBot(commands.Bot):
     def __init__(self, command_prefix, self_bot):
         commands.Bot.__init__(self, command_prefix=command_prefix, self_bot=self_bot)
